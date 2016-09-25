4fd794359d4bfd4835ae7b113729df9a291bc689
janhridel <hridel@gmail.com>
2016-09-25 11:35:31 +0200

    Razeni

---
+++ Přednáška 01/03_Razeni.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_Razeni
+{
+    class Osoba
+    {
+        public string Jmeno { get; set; }
+        public string Prijmeni { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Osoba[] osoby = new Osoba[] {
+			new Osoba { Jmeno = "Jana", Prijmeni = "Nováková" },
+			new Osoba { Jmeno = "Václav", Prijmeni = "Hřídel" },
+			new Osoba { Jmeno = "Adéla", Prijmeni = "Nováková" },
+			new Osoba { Jmeno = "Josef", Prijmeni = "Hřídel" } };
+            IEnumerable<Osoba> serazeneOsoby = osoby.OrderBy(x => x.Prijmeni).ThenBy(x => x.Jmeno);
+            foreach (var item in serazeneOsoby)
+            {
+                Console.WriteLine("{0} {1}", item.Prijmeni, item.Jmeno);
+            }
+            Console.ReadKey();
+        }
+    }
+}
