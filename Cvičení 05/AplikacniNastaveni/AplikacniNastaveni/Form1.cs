964943eec0b30b53a8266ec49f4cbeaf62a4f7fa
janhridel <hridel@gmail.com>
2016-10-31 15:07:24 +0100

    Aplikacni nastaveni
    Cviceni 05

---
+++ Cvičení 05/AplikacniNastaveni/AplikacniNastaveni/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AplikacniNastaveni
+{
+
+    public partial class Form1 : Form
+    {
+        MainFormCfg cfg = new MainFormCfg();
+        public Form1()
+        {
+            InitializeComponent();
+            // není nutný test, pokud vlatnost má atribut DefaultSettingValue
+            //if (!cfg.IsLocationNull) Location = cfg.Location;
+            StartPosition = FormStartPosition.Manual;
+            Location = cfg.Location;
+            if (cfg.SizeCfg != null)
+            {
+                Size = cfg.SizeCfg.Size;
+            }
+        }
+
+        protected override void OnFormClosed(FormClosedEventArgs e)
+        {
+            base.OnFormClosed(e);
+            if (cfg.SizeCfg == null) cfg.SizeCfg = new FormSizeCfg();
+            cfg.SizeCfg.Size = Size;
+            cfg.Location = Location;
+            cfg.Save();
+        }
+    }
+}
---
+++ Cvičení 05/AplikacniNastaveni/AplikacniNastaveni/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Drawing;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AplikacniNastaveni
+{
+    public class FormSizeCfg // Třída musí být veřejná kvůli XML serializaci.
+    {
+        public Size Size { get; set; }
+    }
+
+    class MainFormCfg : ApplicationSettingsBase
+    {
+        [UserScopedSetting]
+        [DefaultSettingValue("600, 100")]
+        public Point Location
+        {
+            get { return (Point)this["Location"]; }
+            set { this["Location"] = value; }
+        }
+
+        [UserScopedSetting]
+        public FormSizeCfg SizeCfg
+        {
+            get { return (FormSizeCfg)this["SizeCfg"]; }
+            set { this["SizeCfg"] = value; }
+        }
+
+        public bool IsLocationNull { get { return this["Location"] == null; } }
+    }
+
+
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
