7ef6711018e2ae1bf3ed312c8332cd826684aae8
janhridel <hridel@gmail.com>
2016-12-11 20:37:06 +0100

    Upload Profile Image example

---
+++ Přednáška 12/ImgUpload/HomeController.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ImgUpload.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Profile()
+        {
+            ViewBag.Message = "Your profile picture";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult FileUpload(HttpPostedFileBase file)
+        {
+            if (file != null)
+            {
+                string pic = System.IO.Path.GetFileName(file.FileName);
+                //string path = System.IO.Path.Combine(Server.MapPath("~/images/profile"), pic);
+                string path = System.IO.Path.Combine(Server.MapPath("~/images/profile"), "profile.jpg");
+
+                // file is uploaded
+                file.SaveAs(path);
+
+                // save the image path path to the database or you can send image
+                // directly to database
+                // in-case if you want to store byte[] ie. for DB
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    file.InputStream.CopyTo(ms);
+                    byte[] array = ms.GetBuffer();
+                }
+
+            }
+            // after successfully uploading redirect the user
+            return RedirectToAction("Profile", "Home");
+        }
+    }
+}
